341fabc85b8873304243fdf4407e3ed471cf1811
Muhammad Kamran <kamrandevs@gmail.com>
2023-10-23 15:02:55 +0500

    Duplicate entry blocked in cart

--- app/Http/Controllers/FrontEnd/CartController.php
+++ app/Http/Controllers/FrontEnd/CartController.php
@@ -47,7 +47,12 @@ class CartController extends Controller
         // 		}
 
         $active_cart = Cart::where('user_id', Auth::user()->id)->where('status', 'active')->first();
-        $exsits = Cart_line::where('item_service', $request->item_service)->exists();
+        $exsits = Cart_line::where(
+            [
+                'item_service' => $request->item_service,
+                'item_price' => $request->item_price
+            ]
+        )->exists();
 
         if (!$exsits) {
             if (isset($active_cart->id)) {
