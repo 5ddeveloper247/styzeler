405e93081ebb49fa920a973b8ea6f5938d1d1d09
sunshinelover404 <sunshinelover404@gmail.com>
2023-11-28 16:54:14 +0500

    FixedCartIssueContactNumberIssueAndCronIssueONholdBooking

--- resources/views/log.blade.php
+++ resources/views/log.blade.php
@@ -10,6 +10,7 @@
 <body>
     <h1>Styzler Cron_____Log Viewer</h1>
     <h2 style="color:red">Guide to look into Logs:</h2>
+    <h2 style="color:rgb(220, 20, 160)">Cron only cancel the booking whose difference time is less than 12 hours</h2>
     <h3>Look into the end of the file for latest log each log started with start time(latest time) when it stated and end time when it ended, Btw these red and green text what cron does it list here <b style="color:red">checkOnHold function started</b><b style="color:green">checkOnHold function finishe</b></h3>
     <pre>{{ $logContent }}</pre>
    
